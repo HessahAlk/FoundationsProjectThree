3306aa6009f134bc51716cb87d74937e1cead805
HessahAlK <hessah.alkhudhair@gmail.com>
2018-10-04 16:15:07 +0300

    Project 3 submition

--- actions.py
+++ actions.py
@@ -2,35 +2,85 @@
 from data import  population, clubs
 from components import Club, Person
 
-my_name = ""
+my_name = "The Gym"
 my_age = -1
 my_bio = ""
 myself = Person(my_name, my_bio, my_age)
 
 def introduction():
-    print("Hello, %s. Welcome to our portal." % my_name)
+	print("Hello, %s. Welcome to our portal." % my_name)
 
 def options():
-    # your code goes here!
-    
+	print("          ")
+	print("Would you like to:")
+	print("1) Create a new club.")
+	print("or:")
+	print("2) Browse and join clubs.")
+	print("or:")
+	print("3) View existing clubs.")
+	print("or:")
+	print("4) Display members of a club.")
+	print("or:")
+	print("-1) Close application.")
+	return input("> ")
+	
 
 def create_club():
-    # your code goes here!
-    
+	name = input("Pick a name for your awesome new club: ")
+	description = input("What is your club about?\n")
+	new_club = Club(name, description)
+	new_club.recruit_member(myself)
+	new_club.assign_president(myself)
+	print("Enter the numbers of the people you would like to recruit to your new club (-1 to stop):")
+	print_population()
+	num = 0
+	while num != "-1":
+		num = input("> ")
+		if num.isdigit() and int(num) <= len(population):
+			person = population[int(num)-1]
+			new_club.recruit_member(person)
+
+	print("Here's your club:")
+	print(new_club.name)
+	print(new_club.description)
+	new_club.print_member_list()
+	clubs.append(new_club)
+	
 
 def view_clubs():
-    # your code goes here!
-    
+	for club in clubs:
+		print("\tNAME: %s\n\tDESCRIPTION: %s\n\tMEMBERS: %s\n" % (club.name, club.description, len(club.members)))
+	
 
 def view_club_members():
-    # your code goes here!
-    
+	view_clubs()
+	club_name = input("Enter the name of the club whose members you'd like to see: ")
+	for club in clubs:
+		if club.name == club_name:    
+			club.print_member_list()
+	
 
 def join_clubs():
-    # your code goes here!
-    
+	view_clubs()
+	club_name = input("Enter the name of the club you'd like to join: ")
+	for club in clubs:
+		if club.name == club_name:
+			club.recruit_member(myself)
+			print("%s just joined %s!" % (myself.name, club.name))
+	
 
 def application():
-    introduction()
-    # your code goes here!
-    
+	introduction()
+	option = ""
+	while option != "-1":
+		option = options()
+		if option.isdigit():
+			if int(option) == 1:
+				create_club()
+			elif int(option) == 2:
+				join_clubs()
+			elif int(option) == 3:
+				view_clubs()
+			elif int(option) == 4:
+				view_club_members()
+	
--- components.py
+++ components.py
@@ -1,22 +1,38 @@
 # CLASSES AND METHODS
 class Person():
     def __init__(self, name, bio, age):
-        # your code goes here!
+        self.name = name
+        self.bio = bio
+        self.age = age
 
 
 class Club():
     def __init__(self, name, description):
-        # your code goes here!
-asdfghjkl;
+        self.president = None
+        self.members = []
+        self.name = name
+        self.description = description
+
 
     def assign_president(self, person):
-        # your code goes here!
+        if person in self.members:
+            self.president = person
+        else:
+            print("%s is not a member of this club. The president must first be a member before being assigned as the president." % person.name)
 
 
     def recruit_member(self, person):
-        # your code goes here!
+        if person not in self.members:
+            self.members.append(person
 
 
     def print_member_list(self):
-        # your code goes here!
+        print("Members: ")
+        for person in self.members:
+            if person is self.president:
+                print("- %s (%s years old, President) - %s" % (person.name, person.age, person.bio))
+            else:
+                print("- %s (%s years old) - %s" % (person.name, person.age, person.bio))
+            print()
 
+        print("Average age in this club: %syr" % self.get_average_age())
